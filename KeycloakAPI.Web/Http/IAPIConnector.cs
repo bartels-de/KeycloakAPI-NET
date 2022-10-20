e89e87af1ea307bb95132cb6a49fbd90bd7e0d99
Jannik Bartels <66883009+bartels-de@users.noreply.github.com>
2022-10-20 17:27:13 +0200

    Projektdateien hinzufügen.

---
+++ KeycloakAPI.Web/Http/IAPIConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycloakAPI.Web.Http
+{
+    internal interface IAPIConnector
+    {
+        Task<T> Get<T>(Uri uri);
+        Task<T> Get<T>(Uri uri, IDictionary<string, string>? parameters);
+
+        Task<T> Post<T>(Uri uri);
+        Task<T> Post<T>(Uri uri, IDictionary<string, string>? parameters);
+        Task<T> Post<T>(Uri uri, IDictionary<string, string>? parameters, object? body);
+
+        Task<T> SendAPIRequestAsync<T>(
+            Uri uri, 
+            HttpMethod method,
+            IDictionary<string, string>? parameters = null,
+            object? body = null,
+            IDictionary<string, string>? headers = null);
+    }
+}
